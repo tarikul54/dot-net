d8bf10bf79945be92a0408e62fcd48b92278a01f
tarikul54 <68778755+tarikul54@users.noreply.github.com>
2022-05-29 02:39:55 +0600

    Add files via upload

---
+++ Calculator 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Calculator_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine(" WELLCOME TO CALCULATOR");
+                Console.Write("\n Enter Your First Number : ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write(" Enter Your Second Numeber : ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\n 1.For Addition(+) Type : 1,\n 2.For Subtraction(-) Type : 2,\n 3.For Multiplication(*) Type : 3,\n 4.For Division(/) Type : 4 ,\n 5.For Remender(%) Type : 5 ");
+                Console.Write("\n Enter Your Operator Number : ");
+                int time = Convert.ToInt32(Console.ReadLine());
+
+                if (time == 1)
+                {
+                    Console.WriteLine("\n Result = " + (num1 + num2));
+                }
+                else if (time == 2)
+                {
+                    Console.WriteLine("\n Result = " + (num1 - num2));
+                }
+                else if (time == 3)
+                {
+                    Console.WriteLine("\n Result = " + (num1 * num2));
+                }
+                else if (time == 4)
+                {
+                    Console.WriteLine("\n Result = " + (num1 / num2));
+                }
+            else if (time == 5)
+            {
+                Console.WriteLine("\n Result = " + (num1 % num2));
+            }
+            else
+                {
+                Console.WriteLine("\n Your Selected Operator  is wrong\n Please Type 1 for Addition, 2 For Subtraction, 3  For Multiplication, 4 For Division, 5 Remender");
+                }
+         
+            }            
+        }
+    }
+
+
+    
+
---
+++ dot net2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dot_net2
+{
+    class Program
+    {
+        private const string Format = "your sge is:";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Your age: " + age);
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+            float v = (float)Convert.ToDouble(Console.ReadLine());
+            NewMethod(v);
+        }
+
+        private static void NewMethod(float v)
+        {
+            var age = v;
+        }
+    }
+}
+    
